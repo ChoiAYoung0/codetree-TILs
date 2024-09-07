340b8be4287fc35825124801b0678411ae0e1143
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2024-09-07 22:39:55 +0900

    Title: 체질량지수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240907/체질량지수/bmi.c
+++ 240907/체질량지수/bmi.c
@@ -4,7 +4,7 @@ int main() {
     int h,w
     scanf("%d%d",&h,&w);
     printf("%d",(10000*w)/(h*h));
-    if(10000*w/h*h>=25)
+    if((10000*w/h*h)>=25)
         printf("\nObesity");
     return 0;
 }
