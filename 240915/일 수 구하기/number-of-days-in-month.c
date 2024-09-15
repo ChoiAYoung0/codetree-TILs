bc1f718c25409159180dcac6add22f9d5fb03ff8
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2024-09-15 12:53:59 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240915/일 수 구하기/number-of-days-in-month.c
+++ 240915/일 수 구하기/number-of-days-in-month.c
@@ -5,16 +5,16 @@ int main() {
     scanf("%d",&n);
     if(n%2!=0&&n<8||n%2==0&&n>=8)
     {
+        printf("31");
+    }
+    else
+    {
         if(n==2)
         {
             printf("28");
         }
         else
-            printf("31");
-    }
-    else
-    {
-        printf("30");
+            printf("30");
     }
 
     
