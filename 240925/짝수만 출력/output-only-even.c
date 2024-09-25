372ed2e2b900999a94f6d152f24a328197d1b11d
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2024-09-25 10:41:05 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240925/짝수만 출력/output-only-even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    int i = a;
+    while(i<=b)
+    {
+        if(i%2==0)
+        {
+            printf("%d ",i);
+        }
+        i++;
+    }
+    return 0;
+}
