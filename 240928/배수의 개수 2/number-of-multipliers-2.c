db5a2974a208aeb6e2b585f35535781c5f80b61d
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2024-09-28 12:07:26 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240928/배수의 개수 2/number-of-multipliers-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int cnt=0;
+    int a;
+    for(int i = 0 ; i<10; i++)
+    {
+        scanf("%d",&a);
+        if(a%2==1)
+            cnt++;
+    }
+    printf("%d",cnt);
+    return 0;
+}
