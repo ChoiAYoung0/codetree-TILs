007ec3282d0fac3e788565460939cb701659c3d6
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2024-09-28 12:18:28 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240928/청소당번 정하기/cleaning-numbering.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int cntTwo=0, cntThree=0, cntTwelve=0;
+    scanf("%d",&n);
+    cntTwo = n/2;
+    cntThree = n/3;
+    cntTwelve = n/12;
+    printf("%d %d %d",cntTwo-n/6-cntTwelve, cntThree-cntTwelve, cntTwelve);
+
+    return 0;
+}
