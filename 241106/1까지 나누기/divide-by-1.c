351178a754039fbda47273bc541b8430c2dea63b
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2024-11-06 11:02:46 +0900

    Title: 1까지 나누기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241106/1까지 나누기/divide-by-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=1 ;i<5000 ; i++)
+    {
+        n = n/i;
+        if(n<=1)
+        {
+            printf("%d",i);
+            break;
+        }
+    }
+    return 0;
+}
