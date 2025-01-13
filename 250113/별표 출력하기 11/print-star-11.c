678f8653503b4f6e49d3adeeedd7ceee31a26aa7
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-13 13:09:12 +0900

    Title: 별표 출력하기 11, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250113/별표 출력하기 11/print-star-11.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    for(int i=0 ; i<=n*2; i++)
+    {
+        for(int j=0 ; j<=n*2 ; j++)
+        {
+            if(i%2!=0 && j%2!=0)
+            {
+                printf("  ");
+            }
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
