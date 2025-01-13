0cfd180ce871417608772c84d0d0987cd566d8c1
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-13 15:43:37 +0900

    Title: 1-9 정사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250113/1-9 정사각형/1-9-square.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int cnt=1;
+    scanf("%d",&n);
+    for(int i=0 ; i<n ; i++)
+    {
+        for(int j=0 ; j<n ; j++)
+        {
+            if(cnt>9)
+                cnt = 1;
+            
+            printf("%d",cnt);
+            cnt++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
