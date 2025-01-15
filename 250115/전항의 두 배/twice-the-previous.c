7d6cef4bb2289253764aa8e94ce2f82fb5658f30
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-15 13:05:53 +0900

    Title: 전항의 두 배, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250115/전항의 두 배/twice-the-previous.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int arr[10];
+    scanf("%d %d",&arr[0],&arr[1]);
+    printf("%d %d ",arr[0],arr[1]);
+    for(int i=2 ; i<10 ; i++)
+    {
+        arr[i] = arr[i-1] + 2*arr[i-2];
+        printf("%d ",arr[i]);
+    }
+    return 0;
+}
