ed85b52a92d0177628abbe3500aed4ceab514129
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-16 13:40:22 +0900

    Title: n개의 정수 중 최대 2개, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250116/n개의 정수 중 최대 2개/two-max-of-n-num.c
+++ 250116/n개의 정수 중 최대 2개/two-max-of-n-num.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+#include <limits.h>
 
 int main() {
     int n;
     int arr[100];
-    int max1, max2, max1idx = 0;
+    int max1, max1idx = 0;
+    int max2 = INT_MIN;
     scanf("%d",&n);
     for(int i=0 ; i<n ; i++)
     {
         scanf("%d",&arr[i]);
     }
     max1 = arr[0];
-    max2 = arr[0];
     for(int i=0 ; i<n ; i++)
     {
         if(max1<arr[i])
@@ -23,6 +24,7 @@ int main() {
     {
         if(i == max1idx)
             continue;
+        
         if(max2<arr[i])
         {
             max2 = arr[i];
