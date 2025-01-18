b72b2b434b7e89159e52020c62e0ef396c8f9ca4
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-18 19:51:03 +0900

    Title: 문자열의 총 길이 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250118/문자열의 총 길이 구하기/find-the-length-of-the-string.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char arr[10][21];
+    int sum=0;
+    for(int i=0 ; i<10 ; i++)
+    {
+        scanf("%s",&arr[i]);
+    }
+
+    for(int i=0 ; i<10 ; i++)
+    {
+        sum += strlen(arr[i]);
+    }
+    
+    printf("%d",sum);
+
+    return 0;
+}
