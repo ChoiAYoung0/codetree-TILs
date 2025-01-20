a1f1d3099af1a0ad54fd7f0a96601c13a7983945
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-20 14:06:20 +0900

    Title: 규칙에 따라 밀기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250120/규칙에 따라 밀기/push-by-the-rules.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char A[101],B[101];
+    char temp;
+    int len,lenB;
+
+    scanf("%s%s",A,B);
+    len = strlen(A);
+    lenB = strlen(B);
+
+    for(int i=0 ; i<len ; i++)
+    {
+        if(B[i]=='L')
+        {
+            temp = A[0];
+            for(int i=0 ; i<len-1 ; i++)
+            {
+                A[i] = A[i+1];
+            }
+            A[len-1] = temp;
+        }
+        if(B[i]=='R')
+        {
+            temp = A[len-1];
+            for(int i=len-1 ; i>0 ; i--)
+            {
+                A[i] = A[i-1];
+            }
+            A[0] = temp;
+        }
+    }
+
+
+    printf("%s",A);
+    
+    
+
+
+}
