40b594f864e42d34039d22e9f595d26319200a20
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-28 23:06:36 +0900

    Title: 파스칼의 삼각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250128/파스칼의 삼각형/pascals-triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    int arr[15][15];
+    
+    scanf("%d",&n);
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=i ; j++)
+        {
+            if(j==0 || j==i)
+            {
+                arr[i][j] = 1;
+            }
+            else
+            {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+            }
+        }
+    }
+
+    for(int i=1 ; i<=n ; i++)
+    {
+        for(int j=1 ; j<=i ; j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
