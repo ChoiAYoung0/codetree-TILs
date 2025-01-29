08ed57aa99704da75fb25792397e6cdbca962688
A Young Choi <105281883+ChoiAYoung0@users.noreply.github.com>
2025-01-29 13:12:09 +0900

    Title: 단어로 구분하기 3, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 250129/단어로 구분하기 3/separate-words-with-words-3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    char arr[10][201];
+
+    for(int i=0 ; i<10 ; i++)
+    {
+        scanf("%s",arr[i]);
+    }
+
+    for(int i=9 ; i>=0 ; i--)
+    {
+        printf("%s\n",arr[i]);
+    }
+
+
+    return 0;
+}
